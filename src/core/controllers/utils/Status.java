7d10c8a1bb0fd65850284fa5c11a64ed7bbe07ca
luencaro <luencaro2005@gmail.com>
2024-05-19 19:32:21 -0500

    Responses and Status Added

--- src/core/controllers/utils/Status.java
+++ src/core/controllers/utils/Status.java
@@ -9,5 +9,16 @@ package core.controllers.utils;
  * @author Sebastián
  */
 public class Status {
+    // Successful response
+    public static final int OK = 200;
+    public static final int CREATED = 201;
+    public static final int NO_CONTENT = 204;
     
+    // Client error response
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+    
+    // Server error response
+    public static final int INTERNAL_SERVER_ERROR = 500;
+    public static final int NOT_IMPLEMENTED = 501;
 }
